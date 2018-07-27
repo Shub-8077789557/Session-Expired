056838711b0ce50bb349ef7f29efee044ffb140d
shubham-bhardwaj17 <shubham.8077789557@gmail.com>
2018-07-27 11:47:53 +0530

    Session Expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef } from '@angular/core';
+import {Keepalive} from '@ng-idle/keepalive';
+import {Idle, DEFAULT_INTERRUPTSOURCES} from '@ng-idle/core';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  idleState;
+  timedOut = false;
+  timeWarn = false;
+  lastPing?: Date = null;
+  OnState;
+
+  constructor(private idle: Idle, private keepalive: Keepalive, private router: Router) {
+    // sets an idle timeout of 5 seconds, for testing purposes.
+    idle.setIdle(5);
+    // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
+    idle.setTimeout(5);
+    // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
+    idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+    
+   
+    //idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
+    idle.onTimeout.subscribe(() => {
+     alert('Session has been Expired!!');
+      this.timedOut = true;
+      this.timeWarn = false;
+      this.router.navigate(['logout']);
+    });
+    //idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
+    idle.onTimeoutWarning.subscribe((countdown) => {
+      this.idleState ='You will time out in ' + countdown + ' seconds!';
+      this.timeWarn = true;
+    });
+
+    // sets the ping interval to 15 seconds
+    keepalive.interval(15);
+
+    keepalive.onPing.subscribe(() => this.lastPing = new Date());
+
+    this.reset();
+   
+  }
+
+
+  reset() {
+    this.idle.watch();
+    this.OnState = this.router.navigate(['products']); 
+    this.timedOut = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {NgIdleKeepaliveModule} from '@ng-idle/keepalive';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
-
-
+import { MomentModule } from 'angular2-moment';
+import { ProductsComponent } from './products/products.component';
+import {RouterModule} from '@angular/router';
+import { LogoutComponent } from './logout/logout.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LogoutComponent, 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgIdleKeepaliveModule.forRoot(),NgbModule.forRoot(),
+     HttpModule, MomentModule, FormsModule, RouterModule.forRoot([
+      //{path:'', redirectTo:'products', pathMatch:'full'},
+      {path:'products', component:ProductsComponent}, 
+      {path:'logout', component:LogoutComponent}
+     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
